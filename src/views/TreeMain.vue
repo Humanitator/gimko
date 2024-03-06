e2c0c31d3e672e1b53b6d258a688e5206629bf3b
Humanitator <andris.selavo@gmail.com>
2024-03-06 20:31:00 +0200

    Fixed tree adding

--- src/views/TreeMain.vue
+++ src/views/TreeMain.vue
@@ -2,7 +2,7 @@
     import { onMounted, ref } from 'vue';
     import { getAuth } from 'firebase/auth';
     import db from "../firebase/init.js";
-    import { defaultTree, defaultTreePrivates } from "../firebase/defaultStructs"
+    import { defaultTree, defaultTreePrivate } from "../firebase/defaultStructs"
     import { doc, addDoc, setDoc, getDoc, getDocs, query, collection, updateDoc, where } from "firebase/firestore";
     import { useRouter } from 'vue-router';
 
@@ -48,8 +48,6 @@
             newTreeName.value = "Jauns koks";
         }
 
-        console.log("test3")
-
         const treeColRef = collection(db, 'trees');
         // Check if tree exists
         const q = query(treeColRef, where('name', '==', newTreeName.value));
@@ -59,17 +57,14 @@
             return;
         }
 
-        console.log("test2")
-
         // Make new tree
         const treeObj = structuredClone(defaultTree);
         treeObj.name = newTreeName.value;
         treeObj.ownerID = auth.currentUser.uid;
-
         const treeRef = await addDoc(treeColRef, treeObj);
-        await setDoc(doc(db, "data", ("/trees/" + treeRef.id + "/private"), structuredClone(defaultTreePrivates)));
         
-        console.log("test")
+        const treePrivatesObj = structuredClone(defaultTreePrivate);
+        await setDoc(doc(db, ("/trees/" + treeRef.id + "/private"), "data"), treePrivatesObj);
 
         // Get user trees
         const userRef = doc(db, "users", auth.currentUser.uid);
